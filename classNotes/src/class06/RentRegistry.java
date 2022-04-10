34c4d6f6d569d833fb4a9a55cc4324e1fe7ffb3e
João Victor Carvalho <joaocarvoli@hotmail.com>
2022-04-09 23:28:54 -0300

    Update RentRegistry.java

--- classNotes/src/class06/RentRegistry.java
+++ classNotes/src/class06/RentRegistry.java
@@ -4,15 +4,9 @@ public class RentRegistry {
     Car car; // points to null
     int valueRent;
 
-    public void setCar(Car carReference){
-        this.car = carReference; // now, car points to the car from the Main function
-    }
+    public void setCar(Car carReference) {this.car = carReference;} // now, the car variable points to the car from the Main function whose the reference was passed
 
-    public void setValueRent(int value){
-        this.valueRent = value;
-    }
+    public void setValueRent(int value) {this.valueRent = value;}
 
-    public int getValueRent(){
-        return this.valueRent;
-    }
+    public int getValueRent() {return this.valueRent;}
 }
